db46ab4b0014e6f9f7d3cbe4785a91572eccd30b
Takumi-ochiai <69828894+Takumi-ochiai@users.noreply.github.com>
2020-09-11 19:52:59 +0900

    user.rbバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable  
+
+  with_options presence: true do
+    validates :nickname, presence: true
+    validates :password, format: { with: /[a-z\d]{8,}/i, message: "include both letters and numbers"}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width caracters"}
+    validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana caracters"}
+    validates :first_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width katakana caracters"}
+    validates :birthday, presence: true
+  end
 end
