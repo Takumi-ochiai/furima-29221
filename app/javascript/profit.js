2835f9a918197cef329a62717baf96648fa426b4
Takumi-ochiai <69828894+Takumi-ochiai@users.noreply.github.com>
2020-09-18 16:04:53 +0900

    販売手数料、利益表示設定

---
+++ app/javascript/profit.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+
+  const textField = document.getElementById("item-price")
+  const addTax = document.getElementById("add-tax-price")
+  const profit = document.getElementById("profit")
+
+  textField.addEventListener("change", function(){
+    const value = textField.value
+    addTax.textContent = value * 0.1
+    profit.textContent = value * 0.9
+  })
+
+})
