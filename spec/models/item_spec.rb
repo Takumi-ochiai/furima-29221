1706da4f7f1f734f7e29f60ae75835e3f7ac261a
Takumi-ochiai <69828894+Takumi-ochiai@users.noreply.github.com>
2020-09-20 16:53:49 +0900

    表示順をid昇順に設定

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,64 +6,64 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品機能' do
     context '商品出品が上手くいかない時'
-      it '商品名がなければ登録できない' do
-        @item.name = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
-      end
-      it '商品説明がなければ登録できない' do
-        @item.description = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank")
-      end
-      it '値段がなければ登録できない' do
-        @item.price = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
-      end
-      it '値段は半角でなければ登録できない' do
-        @item.price = '３００'
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price Half-width number')
-      end
-      it '値段が¥300未満だと登録できない' do
-        @item.price = '100'
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range')
-      end
-      it '値段が¥9999999より大きいと登録できない' do
-        @item.price = '10000000'
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range')
-      end
-      it 'カテゴリーがなければ登録できない' do
-        @item.category_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Category must be other than 1')
-      end
-      it '商品の状態がなければ登録できない' do
-        @item.state_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include('State must be other than 1')
-      end
-      it '配送料の負担の選択がなければ登録できない' do
-        @item.delivery_charge_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery charge must be other than 1')
-      end
-      it '発送元の地域がなければ登録できない' do
-        @item.shipping_area_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
-      end
-      it '発送までの日数が存在すれば登録できる' do
-        @item.shipping_day_id = 1
-        @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
-      end
-      context '新規登録が上手くいくとき'
-      it 'image,name,description,price,category,state,delivery_change,shipping_area,shipping_dayが存在すれば登録できる' do
+    it '商品名がなければ登録できない' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it '商品説明がなければ登録できない' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+    it '値段がなければ登録できない' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it '値段は半角でなければ登録できない' do
+      @item.price = '３００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price Half-width number')
+    end
+    it '値段が¥300未満だと登録できない' do
+      @item.price = '100'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price Out of setting range')
+    end
+    it '値段が¥9999999より大きいと登録できない' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price Out of setting range')
+    end
+    it 'カテゴリーがなければ登録できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
+    end
+    it '商品の状態がなければ登録できない' do
+      @item.state_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('State must be other than 1')
+    end
+    it '配送料の負担の選択がなければ登録できない' do
+      @item.delivery_charge_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Delivery charge must be other than 1')
+    end
+    it '発送元の地域がなければ登録できない' do
+      @item.shipping_area_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
+    end
+    it '発送までの日数が存在すれば登録できる' do
+      @item.shipping_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
+    end
+    context '新規登録が上手くいくとき'
+    it 'image,name,description,price,category,state,delivery_change,shipping_area,shipping_dayが存在すれば登録できる' do
       expect(@item).to be_valid
-      end
+    end
   end
 end
