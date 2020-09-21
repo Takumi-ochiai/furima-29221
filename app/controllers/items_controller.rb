6a3c22bf4dbe1d9d14bab8da54f9967b031688c0
Takumi-ochiai <69828894+Takumi-ochiai@users.noreply.github.com>
2020-09-21 19:57:57 +0900

    rubocop使用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.order(id: "DESC")
+    @items = Item.all.order(id: 'DESC')
   end
 
   def new
@@ -18,7 +18,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-   
   end
 
   private
@@ -26,5 +25,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :state_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
-
 end
