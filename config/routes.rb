cf716c5d0c2a6288b0ce6efec624deed9a2111dd
Takumi-ochiai <69828894+Takumi-ochiai@users.noreply.github.com>
2020-09-22 11:30:31 +0900

    商品情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # rootの設定をしましょう
   root to: 'items#index'
-  resources :items, only: [:edit, :new, :show, :create, :destroy]
+  resources :items, only: [:edit, :update, :new, :show, :create, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
